6a5de2ebb2be05f5f8c20f4cfe06b8607f0921a6
p16238436 <p16238436@GWHXX680WIN002W>
2018-04-20 11:33:28 +0100

    added GenreViewModels class

---
+++ MovieReviewWebsite/MovieReviewWebsite/Models/genreviewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieReviewWebsite.Models
+{
+    public class genreviewModels
+    {
+        public Movie Movie { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
